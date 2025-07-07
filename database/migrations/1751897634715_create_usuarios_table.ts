23b322887f3a5bb95404acf1ab52a882f967a576
rycsouza <rychard.souza1111@gmail.com>
2025-07-07 18:00:25 -0400

    migrations

---
+++ database/migrations/1751897634715_create_usuarios_table.ts
@@ -0,0 +1,26 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'usuario'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.timestamp('data_cadastro')
+      table.timestamp('data_atualizacao')
+
+      table.string('nome').notNullable()
+      table.string('email').notNullable().unique()
+      table.string('telefone').unique()
+      table.string('username').unique()
+      table.string('senha')
+      table.string('avatar')
+      table.string('ativo2FA')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
